27cdfbbc36c62fec47f95e592c2b5517044edd28
lapatric <42653152+lapatric@users.noreply.github.com>
2025-09-04 10:22:10 +0200

    workflow fixes

--- test/mocha/unit/rpc/GlobalRpcProviders.test.ts
+++ test/mocha/unit/rpc/GlobalRpcProviders.test.ts
@@ -1,33 +1,5 @@
 import { GlobalRpcProviders } from '../../../../lib/rpc/GlobalRpcProviders'
-import { default as bunyan, default as Logger } from 'bunyan'
-import { ChainId } from '@uniswap/sdk-core'
-import { expect } from 'chai'
-import { SingleJsonRpcProviderConfig, UniJsonRpcProviderConfig } from '../../../../lib/rpc/config'
 import Sinon, { SinonSandbox } from 'sinon'
-import TEST_PROD_CONFIG from './rpcProviderTestProdConfig.json'
-
-const log: Logger = bunyan.createLogger({ name: 'test' })
-
-const UNI_PROVIDER_TEST_CONFIG: UniJsonRpcProviderConfig = {
-  HEALTH_EVALUATION_WAIT_PERIOD_IN_S: 5,
-  ENABLE_SHADOW_LATENCY_EVALUATION: false,
-  LATENCY_EVALUATION_WAIT_PERIOD_IN_S: 15,
-  DEFAULT_INITIAL_WEIGHT: 1000,
-}
-
-const SINGLE_PROVIDER_TEST_CONFIG: SingleJsonRpcProviderConfig = {
-  HEALTH_EVALUATION_WAIT_PERIOD_IN_S: 5,
-  ERROR_PENALTY: -50,
-  HIGH_LATENCY_PENALTY: -50,
-  HEALTH_SCORE_FALLBACK_THRESHOLD: -70,
-  HEALTH_SCORE_RECOVER_THRESHOLD: -10,
-  MAX_LATENCY_ALLOWED_IN_MS: 500,
-  RECOVER_SCORE_PER_MS: 0.005,
-  RECOVER_MAX_WAIT_TIME_TO_ACKNOWLEDGE_IN_MS: 20000,
-  DB_SYNC_INTERVAL_IN_S: 5,
-  LATENCY_STAT_HISTORY_WINDOW_LENGTH_IN_S: 300,
-  LATENCY_EVALUATION_WAIT_PERIOD_IN_S: 15,
-}
 
 const cleanUp = () => {
   GlobalRpcProviders['UNI_RPC_PROVIDERS'] = null
