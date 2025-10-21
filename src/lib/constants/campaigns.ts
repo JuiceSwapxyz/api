b1866815c9d76cb8bad4d9feeeefdd28289e0af0
lapatric <42653152+lapatric@users.noreply.github.com>
2025-10-21 17:39:59 +0200

    update First Squeezer NFT address

--- src/lib/constants/campaigns.ts
+++ src/lib/constants/campaigns.ts
@@ -3,4 +3,4 @@
  * These are public, immutable smart contract addresses deployed on-chain
  */
 
-export const FIRST_SQUEEZER_NFT_CONTRACT = "0xcF62B46fF36a6FcABf4C0056c535A0cA41E7c03b" as const;
+export const FIRST_SQUEEZER_NFT_CONTRACT = "0x428B878cB6383216AaDc4e8495037E8d31612621" as const;
